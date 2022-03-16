6131ed2795b7cab346b49ca2c1cc91d21d56eb4e
VincentHoar <vincent.hoareau@etu.u-paris.fr>
2022-03-16 16:27:02 +0100

    Ajout de la Q1

---
+++ analyse/londresq1.R
@@ -0,0 +1,23 @@
+#Q1. What would you say is the most important issue facing Paris/votre commune today?
+
+##Chargement des packages
+library(sf)
+library(dplyr)
+library(sp)
+library(mapsf)
+library(haven)
+
+#Import du fichier IPSOS
+ipsoslondres = read_sav("data-raw/21-032532-01 Kings UdP London Paris 2021 Survey - London UK SPSS V1_240621_Client Use Only.sav")
+
+#Import du fond de carte
+fondlondres = st_read('data/cartelondres.gpkg')
+
+#Changement de nom dans le fichier IPSOS pour faciliter le merge
+ipsoslondres = ipsoslondres %>%
+  rename(
+    BOROUGH = QMktSize_2_1
+  )
+
+
+#Création d'une dataframe avec le nombre de répondants par modalité de la question, en fonction des EPT
---
+++ analyse/parisq1.R
@@ -0,0 +1,30 @@
+#Chargement des packages
+library(sf)
+library(dplyr)
+library(sp)
+library(mapsf)
+library(haven)
+
+#Import du fichier IPSOS
+IPSOSPARIS = read_sav("data-raw/21-032532-01 Kings UdP London Paris 2021 Survey - Paris FR SPSS V1_240621_Client Use Only.sav")
+
+#Import du fond de carte
+dep <- st_read('data/fondparis.gpkg', layer = "dep")
+ept <- st_read('data/fondparis.gpkg', layer = "ept_sans")
+
+
+
+#Import du fichier csv des communes
+com_ept = read.csv("data/com_ept1.csv", sep = ";")
+
+#Changement de nom dans le fichier IPSOS pour faciliter le merge
+IPSOSPARIS = IPSOSPARIS %>%
+  rename(
+    code_insee = QMktSize_1_1
+  )
+
+#Jointure des deux fichiers précédents
+mergeparisipsos = merge(IPSOSPARIS, com_ept, by = "code_insee")
+
+test = aggregate(mergeparisipsos[,10:32], by=list(mergeparisipsos$ID_EPT),mean)
+
