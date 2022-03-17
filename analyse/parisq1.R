e0ce5015d51311bcefdf97802b26e8cf37f09be2
rCarto <timothee.giraud@cnrs.fr>
2022-03-17 17:16:38 +0100

    update Q1A

--- analyse/parisq1.R
+++ analyse/parisq1.R
@@ -10,7 +10,7 @@ IPSOSPARIS = read_sav("data-raw/21-032532-01 Kings UdP London Paris 2021 Survey 
 
 #Import du fond de carte
 dep <- st_read('data/fondparis.gpkg', layer = "dep")
-ept <- st_read('data/fondparis.gpkg', layer = "ept_sans")
+ept_raw <- st_read('data/fondparis.gpkg', layer = "ept_sans")
 
 
 
@@ -24,18 +24,46 @@ IPSOSPARIS = IPSOSPARIS %>%
   )
 
 #Jointure des deux fichiers précédents
-mergeparisipsos = merge(IPSOSPARIS, com_ept, by = "code_insee")
+ips = merge(IPSOSPARIS, com_ept, by = "code_insee")
 
-#Méthode 1
-#Faire une matrice pour voir le nombre de total de répondants qui disent oui / non. Le faire 23 fois, mais pour comparer c'est compliqué.
-Q1B1 = table(mergeparisipsos$ID_EPT, mergeparisipsos$Q1B_1)
-Q1B1 = as.data.frame.matrix(Q1B1)
-Q1B2 = as.data.frame.matrix(Q1B2)
 
-Q1TEST = merge(Q1, Q1B2)
+# obtenir les labels de la question 1a dans un vecteur
+labs <- levels(as_factor(ips$Q1A))
 
-#Méthode 2
-#tentative d'agréger les 23 modalités en fonction des EPT mais ce n'est pas des valeurs numériques donc je suis bloqué pour la suite et le FUN = lenght équivaut au nombre de répondants par EPT
 
-test = aggregate(mergeparisipsos[,10:32], by=list(mergeparisipsos$ID_EPT),sum)
+# pivoter la table & agréger les poids selons les réponse
+r <- tapply(ips$weight, list(ips$Q1A, ips$ID_EPT), sum)
+# transformer la table
+r <- t(r)
+# Part de chacune des réponses
+r <- 100 * r / rowSums(r, na.rm = T)
+
+# Quelle est la première réponse
+prem <- apply(X = r, MARGIN = 1, FUN = which.max)
+labs[prem]
+# Sans surprise c'est bien le covid
+
+# deuxième réponse (sans covid, other & don't know)
+sec <- (apply(r[,-c(20, 22, 23)], 1, FUN = which.max))
+labs[-c(20, 22, 23)][sec]
+
+# ajouter une colonne avex la 2eme modalité la plus fréquente 
+rt <- as.data.frame(r)
+rt$top <- labs[-c(20, 22, 23)][sec]
+
+# Ajouter une colonne avec la valeur correspondante
+a <- matrix(c(1:nrow(r), sec), nrow = nrow(r), ncol = 2)
+rt$lab <- round(r[,-c(20, 22, 23)][a], 1)
+
+
+
+# joindre à l'objet sf
+rt$id <- row.names(rt)
+ept <- merge(ept_raw, rt, by.x = "ID_EPT", by.y = "id", add.x = T)
+
+
+# carto
+mf_map(ept, "top", "typo", pal  ='Dynamic')
+mf_title("Most important issue (2nd)")
+mf_label(ept, "lab")
 
