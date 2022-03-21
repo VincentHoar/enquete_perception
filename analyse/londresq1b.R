89f13933a6f06cdd6d5dfd2e1d3a2a879ad54cd1
VincentHoar <vincent.hoareau@etu.u-paris.fr>
2022-03-21 11:28:50 +0100

    update b1

--- analyse/londresq1b.R
+++ analyse/londresq1b.R
@@ -24,19 +24,21 @@ ipsl = merge(ipsoslondres, fondlondres, by = "BOROUGH")
 
 # obtenir les labels de la question 1a dans un vecteur (mêmes pour toutes les thématiques de la Q1B)
 labs <- levels(as_factor(ipsl$Q1B_1))
-##Q1B_1
-# pivoter la table & agréger les poids selons les réponse
-rb1 <- tapply(ipsl$weight, list(ipsl$Q1B_1, ipsl$BOROUGH), sum)
-# transformer la table
-rb1 <- t(rb1)
-# Part de chacune des réponses
-rb1 <- 100 * rb1 / rowSums(rb1, na.rm = T)
 
-b1 = as.data.frame(rb1)
-b1$id = row.names(b1)
+##Q1A+B1(Economy)
+# pivoter la table & agréger les poids selons les réponse
+rb11 <- tapply(ipsl$weight, list(ipsl$Q1A_Q1B_loop1_1, ipsl$BOROUGH), sum)
+# transformer la table
+rb11 <- t(rb11)
+# Part de chacune des réponses
+rb11 <- 100 * rb11 / rowSums(rb11, na.rm = T)
+
+b11 = as.data.frame(rb11)
+b11$id = row.names(b11)
 #merge
-mrg <- merge(fondlondres, b1, by.x = "BOROUGH", by.y = "id", add.x = T)
+mrg <- merge(fondlondres, b11, by.x = "BOROUGH", by.y = "id", add.x = T)
 
 #carto
 mf_map(mrg, "1", "choro", leg_title = "Oui c'est un problème\nen %")
-mf_title("Other important issue : The economy / employments opportunities")
+mf_title("Important issue ? The economy / employments opportunities")
+
